59e53f201ba28f15dcbf04ca8b863393326e83d5
ipetrovic11 <ivan@workchain.io>
2018-11-23 09:28:55 +0100

    Removed Android plugin

--- www/touchid.js
+++ www/touchid.js
@@ -1,29 +1,28 @@
-var argscheck = require('cordova/argscheck'),
-               exec = require('cordova/exec');
+const exec = require('cordova/exec');
 
-var touchid = {
-	isAvailable: function(successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
-	},
-	save: function(key,password, userAuthenticationRequired, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key,password, userAuthenticationRequired]);
-	},
-	verify: function(key,message,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
-	},
-	has: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "has", [key]);
-	},
-	delete: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "delete", [key]);
-	},
-	setLocale: function(locale,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
-	},
-	move: function(key, packageName,successCallback, errorCallback){
-    	exec(successCallback, errorCallback, "TouchID", "move", [key,packageName]);
-    }
+function ToucID() {
+}
 
-};
+ToucID.prototype.isAvailable =  function(successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
+}
+ToucID.prototype.save = function(key,password, userAuthenticationRequired, successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "TouchID", "save", [key,password, userAuthenticationRequired]);
+}
+ToucID.prototype.verify = function(key,message,successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
+}
+ToucID.prototype.has = function(key,successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "has", [key]);
+}
+ToucID.prototype.delete = function(key,successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "delete", [key]);
+}
+ToucID.prototype.setLocale = function(locale,successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
+}
+ToucID.prototype.move = function(key, packageName,successCallback, errorCallback){
+	exec(successCallback, errorCallback, "TouchID", "move", [key,packageName]);
+}
 
-module.exports = touchid;
+module.exports = ToucID;
